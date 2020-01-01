d5a47d0bd8292b69dce9cc24137c09dacb64efb6
Sophie McGarity <sophiemcgarity7@gmail.com>
2019-12-31 16:03:58 -0800

    navigation part 2

---
+++ components/HomeComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Home extends Component {
+
+    static navigationOptions = {
+        title: 'Home'
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Home Component</Text>
+            </View>
+        );
+    }
+}
+
+export default Home;
+
